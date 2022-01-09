856cacf9992ad6eb6f3541d0bbbc2896e66cc102
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2022-01-09 13:09:25 +0100

    Add Save Nodes Local And Update Version

--- qortal-ui-core/src/redux/app/actions/node-config.js
+++ qortal-ui-core/src/redux/app/actions/node-config.js
@@ -1,7 +1,10 @@
 // Node Config Actions here...
 import { LOAD_NODE_CONFIG, SET_NODE, ADD_NODE } from '../app-action-types.js'
+import { UI_VERSION } from '../version.js'
 
 const nodeConfigUrl = '/getConfig'
+
+const checkNodes = JSON.parse(localStorage.getItem('myQortalNodes'));
 
 export const doLoadNodeConfig = () => {
 
@@ -17,8 +20,17 @@ export const doLoadNodeConfig = () => {
                     version: ''
                 }
                 nodeConfig.node = data.config.user.node
-                nodeConfig.knownNodes = data.config.user.knownNodes
-                nodeConfig.version = data.config.user.version
+
+                if (checkNodes === null || checkNodes.length === 0) {
+                    var saveNode = [];
+                    saveNode.push(obj1,obj2,obj3,obj4,obj5,obj6);
+                    localStorage.setItem('myQortalNodes', JSON.stringify(saveNode));
+                    nodeConfig.knownNodes = JSON.parse(localStorage.getItem('myQortalNodes'));
+                } else{
+                    nodeConfig.knownNodes = JSON.parse(localStorage.getItem('myQortalNodes'));
+                }
+
+                nodeConfig.version = UI_VERSION;
                 return dispatch(loadNodeConfig(nodeConfig))
             })
             .catch(err => {
@@ -58,4 +70,46 @@ const addNode = (payload) => {
         type: ADD_NODE,
         payload
     }
+}
+
+const obj1 = {
+    protocol: 'http',
+    domain: '127.0.0.1',
+    port: 12391,
+    enableManagement: true
+}
+
+const obj2 = {
+    protocol: 'http',
+    domain: 'node1.qortal.org',
+    port: 12391,
+    enableManagement: false
+}
+
+const obj3 = {
+    protocol: 'http',
+    domain: 'node2.qortal.org',
+    port: 12391,
+    enableManagement: false
+}
+
+const obj4 = {
+    protocol: 'http',
+    domain: '127.0.0.1',
+    port: 62391,
+    enableManagement: false
+}
+
+const obj5 = {
+    protocol: 'http',
+    domain: 'node1.qortal.org',
+    port: 62391,
+    enableManagement: false
+}
+
+const obj6 = {
+    protocol: 'http',
+    domain: 'node2.qortal.org',
+    port: 62391,
+    enableManagement: false
 }
