24e1cb53f3e19ba04bfb39ceff936354237266fb
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2022-01-23 20:11:46 +0100

    Add chat icon

--- qortal-ui-plugins/plugins/core/components/ChatScroller.js
+++ qortal-ui-plugins/plugins/core/components/ChatScroller.js
@@ -172,7 +172,7 @@ class ChatScroller extends LitElement {
             const myNode = window.parent.reduxStore.getState().app.nodeConfig.knownNodes[window.parent.reduxStore.getState().app.nodeConfig.node];
             const nodeUrl = myNode.protocol + '://' + myNode.domain + ':' + myNode.port;
             const avatarUrl = `${nodeUrl}/arbitrary/THUMBNAIL/${messageObj.senderName}/qortal_avatar?apiKey=${myNode.apiKey}`;
-            avatarImg = `<img src="${avatarUrl}" style="max-width:100%; max-height:100%;" onerror="this.onerror=null;this.style.display='none';" />`;
+            avatarImg = `<img src="${avatarUrl}" style="max-width:100%; max-height:100%;" onerror="this.onerror=null; this.src='/img/standard_chat_avatar.png';" />`;
         }
 
         return `
