6e71849d7548ab9c5611418ba543b9248475919e
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2022-01-24 13:36:30 +0100

    change avatar

--- qortal-ui-plugins/plugins/core/components/ChatPage.js
+++ qortal-ui-plugins/plugins/core/components/ChatPage.js
@@ -231,7 +231,7 @@ class ChatPage extends LitElement {
             const myNode = window.parent.reduxStore.getState().app.nodeConfig.knownNodes[window.parent.reduxStore.getState().app.nodeConfig.node];
             const nodeUrl = myNode.protocol + '://' + myNode.domain + ':' + myNode.port;
             const avatarUrl = `${nodeUrl}/arbitrary/THUMBNAIL/${messageObj.senderName}/qortal_avatar?apiKey=${myNode.apiKey}`;
-            avatarImg = `<img src="${avatarUrl}" style="max-width:100%; max-height:100%;" onerror="this.onerror=null; this.src='/img/standard_chat_avatar.png';" />`;
+            avatarImg = `<img src="${avatarUrl}" style="max-width:100%; max-height:100%;" onerror="this.onerror=null; this.src='/img/incognito.png';" />`;
         }
 
         return `
