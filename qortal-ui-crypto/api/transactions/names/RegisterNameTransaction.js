429ad4a2642ad2af9b738bc4d026ba13a509c972
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2022-02-03 08:43:55 -0800

    change to unitFee

--- qortal-ui-crypto/api/transactions/names/RegisterNameTransaction.js
+++ qortal-ui-crypto/api/transactions/names/RegisterNameTransaction.js
@@ -6,15 +6,6 @@ export default class RegisterNameTransaction extends TransactionBase {
     constructor() {
         super()
         this.type = 3
-        this.fee = 0.001
-        // this.tests.push(
-        //     () => {
-        //         if (!(this._registrantAddress instanceof Uint8Array && this._registrantAddress.length == 25)) {
-        //             return "Invalid registrant " + Base58.encode(this._registrantAddress)
-        //         }
-        //         return true
-        //     }
-        // )
     }
 
     render(html) {
@@ -27,16 +18,19 @@ export default class RegisterNameTransaction extends TransactionBase {
         `
     }
 
+    set fee(fee) {
+        this._fee = fee * QORT_DECIMALS
+        this._feeBytes = this.constructor.utils.int64ToBytes(this._fee)
+    }
+
     set name(name) {
         this.nameText = name;
-
         this._nameBytes = this.constructor.utils.stringtoUTF8Array(name)
         this._nameLength = this.constructor.utils.int32ToBytes(this._nameBytes.length)
     }
 
     set value(value) {
         this.valueText = value.length === 0 ? "Registered Name on the Qortal Chain" : value;
-
         this._valueBytes = this.constructor.utils.stringtoUTF8Array(this.valueText)
         this._valueLength = this.constructor.utils.int32ToBytes(this._valueBytes.length)
     }
