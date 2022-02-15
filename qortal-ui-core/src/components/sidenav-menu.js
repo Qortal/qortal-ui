8ca3f10162a31be8729c86f4551be6b4f9d38f0c
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2022-02-15 18:59:19 +0100

    remove

--- qortal-ui-core/src/components/sidenav-menu.js
+++ qortal-ui-core/src/components/sidenav-menu.js
@@ -244,7 +244,6 @@ class SidenavMenu extends connect(store)(LitElement) {
   async logout(e) {
     if(window.confirm('Are you sure you want to logout?')) {
       store.dispatch(doLogout());
-	  window.location.reload();
     }
   }
 }
