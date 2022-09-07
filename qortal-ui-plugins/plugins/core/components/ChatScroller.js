0528a46b7e430bffb06f731e740c3df6171ab042
Justin Ferrari <justin@codetoo.dev>
2022-09-06 21:23:11 -0500

    Chat Modal Code Refactor

--- qortal-ui-plugins/plugins/core/components/ChatScroller.js
+++ qortal-ui-plugins/plugins/core/components/ChatScroller.js
@@ -1,18 +1,15 @@
-import { LitElement, html, css } from 'lit'
-import { render } from 'lit/html.js'
+import { LitElement, html, css } from 'lit';
+import { render } from 'lit/html.js';
 import { repeat } from 'lit/directives/repeat.js';
-import {  get, translate, } from 'lit-translate'
-import { Epml } from '../../../epml.js'
-
-import './LevelFounder.js'
-import './NameMenu.js'
+import { translate } from 'lit-translate';
+import './LevelFounder.js';
+import './NameMenu.js';
+import './ChatModals.js';
 import '@vaadin/icons';
-import '@vaadin/icon'
-import '@material/mwc-button'
-import '@material/mwc-dialog'
-import '@material/mwc-icon'
-
-const parentEpml = new Epml({ type: 'WINDOW', source: window.parent })
+import '@vaadin/icon';
+import '@material/mwc-button';
+import '@material/mwc-dialog';
+import '@material/mwc-icon';
 
 class ChatScroller extends LitElement {
     static get properties() {
@@ -512,32 +509,11 @@ class MessageTemplate extends LitElement {
                     <chat-menu class="chat-hover" .showPrivateMessageModal=${() => this.showPrivateMessageModal()}></chat-menu>
                 </div>
             </li>
-            <mwc-dialog  
-                id="sendPMDialog"
-                ?open=${this.openDialogPrivateMessage} 
-                scrimClickAction="${this.isLoading ? '' : 'close'}" 
-                escapeKeyAction='close'
-                defaultAction='close'
-                 >
-                <div style="text-align:center">
-                    <h1>${translate("welcomepage.wcchange2")}</h1>
-                    <hr>
-                </div>
-                <p>${translate("welcomepage.wcchange3")}</p>
-                <textarea class="input" ?disabled=${this.isLoading} id="sendTo" rows="1">${this.nametodialog}</textarea>
-                <p style="margin-bottom:0;">
-                    <textarea class="textarea" @keydown=${(e) => this._textArea(e)} ?disabled=${this.isLoading} id="messageBox" placeholder="${translate("welcomepage.wcchange5")}" rows="1"></textarea>
-                </p>
-                <mwc-button ?disabled="${this.isLoading}" slot="primaryAction" @click=${this._sendMessage}>${translate("welcomepage.wcchange6")}</mwc-button>
-                <mwc-button
-                    ?disabled="${this.isLoading}"
-                    slot="secondaryAction"
-                    @click="${() => this.hidePrivateMessageModal()}"
-                    class="close-button"
-                >
-                ${translate("general.close")}
-                </mwc-button>
-            </mwc-dialog>
+            <chat-modals 
+            .openDialogPrivateMessage=${this.openDialogPrivateMessage} 
+            nametodialog="${this.messageObj.senderName ? this.messageObj.senderName : this.messageObj.sender}" 
+            .hidePrivateMessageModal=${() => this.hidePrivateMessageModal()}>
+            </chat-modals>
 		`;
     }
 }
@@ -548,8 +524,6 @@ class ChatMenu extends LitElement {
     static get properties() {
         return {
             menuItems: { type: Array },
-            isLoading: { type: Boolean },
-            nametodialog: { type: String, attribute: true },
             selectedAddress: { type: Object },
             showPrivateMessageModal: {type: Function}
         };
@@ -557,7 +531,6 @@ class ChatMenu extends LitElement {
 
     constructor() {
         super();
-        this.isLoading = false
         this.selectedAddress = window.parent.reduxStore.getState().app.selectedAddress.address
         this.showPrivateMessageModal = () => {};
     }
@@ -630,241 +603,7 @@ class ChatMenu extends LitElement {
             display: block;
             }
 
-            .input {
-                width: 90%;
-                border: none;
-                display: inline-block;
-                font-size: 16px;
-                padding: 10px 20px;
-                border-radius: 5px;
-                resize: none;
-                background: #eee;
-            }
-
-            .textarea {
-                width: 90%;
-                border: none;
-                display: inline-block;
-                font-size: 16px;
-                padding: 10px 20px;
-                border-radius: 5px;
-                height: 120px;
-                resize: none;
-                background: #eee;
-            }
-
-            .close-button {
-                display:block;
-                --mdc-theme-primary: red;
-            }
         `
-    }
-
-    
-    firstUpdated() {
-
-    //     this.changeLanguage()
-    //     this.getChatBlockedAdresses()
-
-	//   setInterval(() => {
-	//       this.getChatBlockedAdresses();
-	//   }, 60000)
-
-        // window.addEventListener('storage', () => {
-        //     const checkLanguage = localStorage.getItem('qortalLanguage')
-        //     use(checkLanguage)
-        // })
-
-        // window.onclick = function(event) {
-        //     if (!event.target.matches('.block')) {
-        //         var dropdowns = document.getElementsByClassName('dropdown-content');
-        //         var i;
-        //         for (i = 0; i < dropdowns.length; i++) {
-        //             var openDropdown = dropdowns[i];
-        //             if (openDropdown.classList.contains('showList')) {
-        //                 openDropdown.classList.remove('showList');
-        //             }
-        //         }
-        //     }
-        // }
-
-        const stopKeyEventPropagation = (e) => {
-            e.stopPropagation();
-            return false;
-        }
-
-        this.shadowRoot.getElementById('sendTo').addEventListener('keydown', stopKeyEventPropagation);
-        this.shadowRoot.getElementById('messageBox').addEventListener('keydown', stopKeyEventPropagation);
-
-        parentEpml.ready().then(() => {
-            parentEpml.subscribe('selected_address', async selectedAddress => {
-                this.selectedAddress = {}
-                selectedAddress = JSON.parse(selectedAddress)
-                if (!selectedAddress || Object.entries(selectedAddress).length === 0) return
-                this.selectedAddress = selectedAddress
-            })
-            parentEpml.request('apiCall', {
-                url: `/addresses/balance/${window.parent.reduxStore.getState().app.selectedAddress.address}`
-            }).then(res => {
-                this.balance = res
-            })
-        })
-        parentEpml.imReady()
-    }
-
-    // Send Private Message
-
-    _sendMessage() {
-        this.isLoading = true
-
-        const recipient = this.shadowRoot.getElementById('sendTo').value
-        const messageBox = this.shadowRoot.getElementById('messageBox')
-        const messageText = messageBox.value
-
-        if (recipient.length === 0) {
-            this.isLoading = false
-        } else if (messageText.length === 0) {
-            this.isLoading = false
-        } else {
-            this.sendMessage()
-        }
-    }
-
-    async sendMessage() {
-        this.isLoading = true
-
-        const _recipient = this.shadowRoot.getElementById('sendTo').value
-        const messageBox = this.shadowRoot.getElementById('messageBox')
-        const messageText = messageBox.value
-        let recipient
-
-        const validateName = async (receiverName) => {
-            let myRes
-            let myNameRes = await parentEpml.request('apiCall', {
-                type: 'api',
-                url: `/names/${receiverName}`
-            })
-
-            if (myNameRes.error === 401) {
-                myRes = false
-            } else {
-                myRes = myNameRes
-            }
-
-            return myRes
-        }
-
-        const myNameRes = await validateName(_recipient)
-        if (!myNameRes) {
-
-            recipient = _recipient
-        } else {
-
-            recipient = myNameRes.owner
-        }
-
-        let _reference = new Uint8Array(64);
-        window.crypto.getRandomValues(_reference);
-
-        let sendTimestamp = Date.now()
-
-        let reference = window.parent.Base58.encode(_reference)
-
-        const getAddressPublicKey = async () => {
-            let isEncrypted
-            let _publicKey
-
-            let addressPublicKey = await parentEpml.request('apiCall', {
-                type: 'api',
-                url: `/addresses/publickey/${recipient}`
-            })
-
-
-            if (addressPublicKey.error === 102) {
-                _publicKey = false
-                // Do something here...
-                let err1string = get("welcomepage.wcchange7")
-                parentEpml.request('showSnackBar', `${err1string}`)
-                this.isLoading = false
-            } else if (addressPublicKey !== false) {
-                isEncrypted = 1
-                _publicKey = addressPublicKey
-                sendMessageRequest(isEncrypted, _publicKey)
-            } else {
-                isEncrypted = 0
-                _publicKey = this.selectedAddress.address
-                sendMessageRequest(isEncrypted, _publicKey)
-            }
-        };
-
-        const sendMessageRequest = async (isEncrypted, _publicKey) => {
-
-            let chatResponse = await parentEpml.request('chat', {
-                type: 18,
-                nonce: this.selectedAddress.nonce,
-                params: {
-                    timestamp: sendTimestamp,
-                    recipient: recipient,
-                    recipientPublicKey: _publicKey,
-                    message: messageText,
-                    lastReference: reference,
-                    proofOfWorkNonce: 0,
-                    isEncrypted: isEncrypted,
-                    isText: 1
-                }
-            })
-            _computePow(chatResponse)
-        }
-
-        const _computePow = async (chatBytes) => {
-
-            const _chatBytesArray = Object.keys(chatBytes).map(function (key) { return chatBytes[key]; });
-            const chatBytesArray = new Uint8Array(_chatBytesArray)
-            const chatBytesHash = new window.parent.Sha256().process(chatBytesArray).finish().result
-            const hashPtr = window.parent.sbrk(32, window.parent.heap);
-            const hashAry = new Uint8Array(window.parent.memory.buffer, hashPtr, 32);
-            hashAry.set(chatBytesHash);
-
-            const difficulty = this.balance === 0 ? 12 : 8;
-
-            const workBufferLength = 8 * 1024 * 1024;
-            const workBufferPtr = window.parent.sbrk(workBufferLength, window.parent.heap);
-
-            let nonce = window.parent.computePow(hashPtr, workBufferPtr, workBufferLength, difficulty)
-
-            let _response = await parentEpml.request('sign_chat', {
-                nonce: this.selectedAddress.nonce,
-                chatBytesArray: chatBytesArray,
-                chatNonce: nonce
-            })
-            getSendChatResponse(_response)
-        }
-
-        const getSendChatResponse = (response) => {
-
-            if (response === true) {
-                messageBox.value = ""
-                let err2string = get("welcomepage.wcchange8")
-                parentEpml.request('showSnackBar', `${err2string}`)
-                this.isLoading = false
-                this.shadowRoot.querySelector('#startPmDialog').close()
-            } else if (response.error) {
-                parentEpml.request('showSnackBar', response.message)
-                this.isLoading = false
-                this.shadowRoot.querySelector('#startPmDialog').close()
-            } else {
-                let err3string = get("welcomepage.wcchange9")
-                parentEpml.request('showSnackBar', `${err3string}`)
-                this.isLoading = false
-                this.shadowRoot.querySelector('#startPmDialog').close()
-            }
-
-        }
-        getAddressPublicKey()
-    }
-
-    _textArea(e) {
-        if (e.keyCode === 13 && !e.shiftKey) this._sendMessage()
     }
 
     render() {
