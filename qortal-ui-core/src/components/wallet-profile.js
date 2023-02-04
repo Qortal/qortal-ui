ea844c9298e4a32f51bfd965e59e4598067c64a0
Pigpig105 <81789882+Pigpig105@users.noreply.github.com>
2023-02-03 19:49:09 -0800

    Update wallet-profile.js

--- qortal-ui-core/src/components/wallet-profile.js
+++ qortal-ui-core/src/components/wallet-profile.js
@@ -12,6 +12,7 @@ class WalletProfile extends connect(store)(LitElement) {
             wallet: { type: Object },
             nodeConfig: { type: Object },
             accountInfo: { type: Object },
+            imageUrl: { type: String },
             theme: { type: String, reflect: true }
         }
     }
@@ -30,6 +31,7 @@ class WalletProfile extends connect(store)(LitElement) {
             names: [],
             addressInfo: {}
         }
+        this.imageUrl = ''
         this.theme = localStorage.getItem('qortalTheme') ? localStorage.getItem('qortalTheme') : 'light'
     }
 
@@ -56,9 +58,9 @@ class WalletProfile extends connect(store)(LitElement) {
                     margin: 0;
                     font-size: 18px;
                     font-weight:500;
-                    display: inline-block;
                     width:100%;
                     padding-bottom:8px;
+                    display: flex;
                 }
 
                 #blocksMinted {
@@ -76,16 +78,37 @@ class WalletProfile extends connect(store)(LitElement) {
                     margin-top:8px;
                     font-size:11px;
                 }
+
+                .round-fullinfo {
+                    position: relative;
+                    width: 69px;
+                    height: 69px;
+                    border-radius: 50%;
+                }
+
+                .full-info-logo {
+                    width: 69px;
+                    height: 69px;
+                    border-radius: 50%;
+                }
+
+                .inline-block-child {
+                    flex: 1;
+                }
             </style>
 
 
             <div id="profileInMenu">
                 <div style="padding: 8px 0;">
-                    <span id="accountName">
-                        ${this.accountInfo.names.length !== 0 ? this.accountInfo.names[0].name : ''}
-                    </span>
-                    ${this.accountInfo.addressInfo ? html`<span style="margin-bottom: 8px; display: inline-block; font-size: 14px;">${translate("walletprofile.minterlevel")} - <span style="color: #03a9f4;">${this.accountInfo.addressInfo.level} ${this.accountInfo.addressInfo.flags === 1 ? html`<strong>(F)</strong>` : ''}</span>` : ''}
-                    <p id="blocksMinted">${translate("walletprofile.blocksminted")} - ${this.accountInfo.addressInfo.blocksMinted + this.accountInfo.addressInfo.blocksMintedAdjustment}</p>
+                    <div id="accountName">
+                        <div id="child inline-block-child" class="full-info-logo">${this.getAvatar()}</div>
+                        &nbsp;&nbsp;&nbsp;
+                        <div id="inline-block-child">
+                            <div>${this.accountInfo.names.length !== 0 ? this.accountInfo.names[0].name : ''}</div>
+                            <div>${this.accountInfo.addressInfo ? html`<span style="margin-bottom: 8px; display: inline-block; font-size: 14px;">${translate("walletprofile.minterlevel")} - <span style="color: #03a9f4;">${this.accountInfo.addressInfo.level} ${this.accountInfo.addressInfo.flags === 1 ? html`<strong>(F)</strong>` : ''}</span>` : ''}</div>
+                            <p id="blocksMinted">${translate("walletprofile.blocksminted")} - ${this.accountInfo.addressInfo.blocksMinted + this.accountInfo.addressInfo.blocksMintedAdjustment}</p>
+                        </div>
+                    </div>
                     <p id="address">${this.wallet.addresses[0].address}</p>
                 </div>
             </div>
@@ -104,6 +127,28 @@ class WalletProfile extends connect(store)(LitElement) {
             toast.verticalOffset = 0
         }
         this.toast = container.appendChild(toast)
+    }
+
+    async getAllWithAddress(myAddress) {
+        await this.getAddressUserAvatar(myAddress)
+    }
+
+    getAvatar() {
+        let numberBlocks = (this.accountInfo.addressInfo.blocksMinted + this.accountInfo.addressInfo.blocksMintedAdjustment);
+        if (Number.isNaN(numberBlocks) || numberBlocks == "" || numberBlocks === null) {
+            return html`<img class="round-fullinfo" src="/img/incognito.png">`
+        } else {
+            const avatarNode = store.getState().app.nodeConfig.knownNodes[store.getState().app.nodeConfig.node]
+            const avatarUrl = avatarNode.protocol + '://' + avatarNode.domain + ':' + avatarNode.port
+            const url = `${avatarUrl}/arbitrary/THUMBNAIL/${this.accountInfo.names[0].name}/qortal_avatar?async=true&apiKey=${this.getApiKey()}`
+            return html`<img class="round-fullinfo" src="${url}" onerror="this.src='/img/incognito.png';" />`
+        }
+    }
+
+    getApiKey() {
+        const apiNode = store.getState().app.nodeConfig.knownNodes[store.getState().app.nodeConfig.node];
+        let apiKey = apiNode.apiKey;
+        return apiKey;
     }
 
     stateChanged(state) {
