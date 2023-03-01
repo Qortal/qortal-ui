42e0200c3332a0c451ba1fa5a83fb58a5fa067a8
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2023-03-01 15:20:23 +0100

    New apt server

--- scripts/add-debian-apt-repo.sh
+++ scripts/add-debian-apt-repo.sh
@@ -21,9 +21,9 @@ if ! which curl; then sudo apt-get --yes install curl; fi
 
 # Install apt repository source list if it does not exist
 if ! grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep qortal.list; then
-  curl -sS https://update.qortal.online/repo/qortal.gpg | sudo apt-key add -
+  curl -sS https://update.qortal.online/qortal.gpg | sudo apt-key add -
   sudo rm -rf /usr/share/keyrings/qortal.gpg
-  sudo apt-key export 7FB37C97 | sudo gpg --dearmour -o /usr/share/keyrings/qortal.gpg
+  sudo apt-key export 856A3FF1 | sudo gpg --dearmour -o /usr/share/keyrings/qortal.gpg
   sudo rm -rf /etc/apt/sources.list.d/qortal.list
-  echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/qortal.gpg] https://update.qortal.online/repo/ ./ ' >  /etc/apt/sources.list.d/qortal.list
+  echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/qortal.gpg] https://update.qortal.online/ ./ ' >  /etc/apt/sources.list.d/qortal.list
 fi
