a2126fc4ce009a4f8a80685e644676ef4918b7f6
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2023-03-03 12:11:39 +0100

    Add build q-app src

--- qortal-ui-plugins/build-config.js
+++ qortal-ui-plugins/build-config.js
@@ -144,6 +144,18 @@ const generateForPlugins = () => {
 			in: 'plugins/core/puzzles/puzzles.src.js',
 			out: 'plugins/core/puzzles/puzzles.js',
 		},
+		{
+			in: 'plugins/core/q-app/q-apps.src.js',
+			out: 'plugins/core/q-app/q-apps.js',
+		},
+		{
+			in: 'plugins/core/q-app/app-browser/app-browser.src.js',
+			out: 'plugins/core/q-app/app-browser/app-browser.js',
+		},
+		{
+			in: 'plugins/core/q-app/publish-app/publish-app.src.js',
+			out: 'plugins/core/q-app/publish-app/publish-app.js',
+		},
 	].map((file) => {
 		return generateRollupConfig(
 			path.join(__dirname, file.in),
