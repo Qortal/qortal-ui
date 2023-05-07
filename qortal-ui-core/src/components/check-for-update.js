8cbf12bec432eacf0c5acd95f8e16ec0df45908f
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2023-05-07 17:38:01 +0200

    Added new search field

--- qortal-ui-core/src/components/check-for-update.js
+++ qortal-ui-core/src/components/check-for-update.js
@@ -1,41 +1,52 @@
 import { LitElement, html, css } from 'lit'
 import { get, translate, translateUnsafeHTML } from 'lit-translate'
+import isElectron from 'is-electron'
 
 import '@polymer/paper-icon-button/paper-icon-button.js'
 import '@polymer/iron-icons/iron-icons.js'
 
 class CheckForUpdate extends LitElement {
-  static get properties() {
-    return {
-      theme: { type: String, reflect: true }
-    }
-  }
+	static get properties() {
+		return {
+			theme: { type: String, reflect: true }
+		}
+	}
 
-  constructor() {
-    super()
-    this.theme = localStorage.getItem('qortalTheme') ? localStorage.getItem('qortalTheme') : 'light'
-  }
+	constructor() {
+		super()
+		this.theme = localStorage.getItem('qortalTheme') ? localStorage.getItem('qortalTheme') : 'light'
+	}
 
-  static styles = [
-    css`
-    `
-  ]
+	static styles = [
+		css`
+		`
+	]
 
-  render() {
-    return html`
-        <div style="display: inline;">
-            <paper-icon-button icon="icons:get-app" @click=${() => this.checkupdate()} title="${translate("appspage.schange38")} UI"></paper-icon-button>
-        </div>
-    `
-  }
+	render() {
+		return html`
+			${this.renderUpdateButton()}
+		`
+	}
 
-  firstUpdated() {
-  }
+	firstUpdated() {
+	}
 
+	renderUpdateButton() {
+		if (!isElectron) {
+			return html`
+				<div style="display: inline;">
+					<paper-icon-button icon="icons:get-app" @click=${() => this.checkupdate()} title="${translate("appspage.schange38")} UI"></paper-icon-button>
+				</div>
+				<div>&nbsp;&nbsp;</div>
+			`
+		} else {
+			return html``
+		}
+	}
 
-  checkupdate() {
-      window.electronAPI.checkForUpdate()
-  }
+	checkupdate() {
+		window.electronAPI.checkForUpdate()
+	}
 }
 
 window.customElements.define('check-for-update', CheckForUpdate)
